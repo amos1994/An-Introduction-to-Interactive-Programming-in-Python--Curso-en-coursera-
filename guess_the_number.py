9167da5b42b82d506c43ce9dbeb829132f07efb5
Alexis Sánchez <aasanchez@gmail.com>
2012-10-26 15:21:44 -0430

    Obtencion de numero secreto e intentos

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,21 +1,31 @@
 # template for "Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
+# Script URL: http://www.codeskulptor.org/#user3-QAwawnboHX-1.py
 
+import simplegui
+import random
+import math
 
 # initialize global variables used in your code
+global secretNumber
+global guesses
 
-
-# define event handlers for control panel
-    
+# define event handlers for control panel    
 def range100():
     # button that changes range to range [0,100) and restarts
-
+    secretNumber = random.randrange(0,100)
+    guesses = 7
+      
 def range1000():
     # button that changes range to range [0,1000) and restarts
+    secretNumber = random.randrange(0,1000)
+    guesses = 10
+
+#def init():
     
-def get_input(guess):
-	# main game logic goes here	
+#def get_input(guess):
+    # main game logic goes here	
 
     
 # create frame
@@ -25,7 +35,6 @@ def get_input(guess):
 
 
 # start frame
-
-
+#init()
 # always remember to check your completed program against the grading rubric
 
